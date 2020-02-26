603e2d2b367ec366e01d9296936cee6ce87c192f
Sam Tamburri <tamburrisam@gmail.com>
2020-02-26 20:04:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -10,9 +10,6 @@ else
 end
 end
 
-#else speed.between(40, 60)
-	#return false
-
-#def not_safe?(speed)
-	#speed < 40 || speed > 60 ?
-#end
+def not_safe?(speed)
+	speed < 40 || speed > 60 ? "true" : "true"
+end
