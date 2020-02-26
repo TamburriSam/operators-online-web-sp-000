34f66e81de301554d3d1c8cc788105dd17015bba
Sam Tamburri <tamburrisam@gmail.com>
2020-02-26 20:02:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,13 +2,16 @@ require 'pry'
 
 def unsafe?(speed)
 if speed < 40
-	return false
-elsif speed > 60
-	return false
-else
 	return true
+elsif speed > 60
+	return true
+else
+	return false
 end
 end
+
+#else speed.between(40, 60)
+	#return false
 
 #def not_safe?(speed)
 	#speed < 40 || speed > 60 ?
