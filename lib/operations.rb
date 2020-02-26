596bcefc243ba1818e0774b537d614f375c13677
Sam Tamburri <tamburrisam@gmail.com>
2020-02-26 19:59:47 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+if speed < 40 
+	return false
+elsif speed > 60
+	return false
+else
+	return true
+end
 end
 
-
+binding.pry
 
 def not_safe?(speed)
-	
+	speed < 40 || > 60 ? 
 end
 	
 
